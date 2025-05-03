8bedf176b40b3045726665545310f65b70686d30
Arsenii Ingannamorte <67146688+IngannamorteScienceDev@users.noreply.github.com>
2025-05-03 16:02:12 +0300

    Add 'Add Drug' button to drug list page

--- meds/urls.py
+++ meds/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('drugs/', views.drug_list, name='drug_list'),
     path('drugs/<int:drug_id>/movement/', views.create_movement, name='create_movement'),
+    path('drugs/create/', views.create_drug, name='create_drug'),
 ]
